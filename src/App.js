33493036ec41aca9c54c04bd3e2931fe02d5eee4
bbarton2 <73309443+bbarton2@users.noreply.github.com>
2022-03-15 16:40:13 -0400

    We have a website that runs now -- bugs gone

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
-import {
-  html,
-  render,
-} from "https://unpkg.com/htm/preact/standalone.module.js";
-import { getSpecials } from "./Services/specials.service.js";
-import { Specials } from "./Components/Specials.js";
-import { Header } from "./Components/Header.js";
-import "./App.css";
+
+import "./styles.css";
 import React, { useState, useEffect } from "react";
 import * as Env from "./enviornment";
 import Parse from "parse";
-
-import { getById } from './Common/LearnService';
-
+import Components from "./Components/Components.js";
 
 Parse.initialize(Env.APPLICATION_ID, Env.JAVASCRIPT_KEY);
 Parse.serverURL = Env.SERVER_URL;
 
 function App() {
-  const [menu, setMenu] = useState([]);
+  return <Components />;
+}
 
-  // the useEffect hook is used here to load user data asynchronously
-  // we will discuss async data in class, for now you can utilize the pattern
-
-  useEffect(() => {
-    return getSpecials().then((data) => {
-      setMenu(data);
-    });
-  }, []);
-  // here is where we put everything together
-  return html`
-  <${Header} title="Specials">
-  </${Header}>
-  <${Specials} menu="${menu}">
-  </${Specials}>
-  `;
-} // in the future we will be adding in a map function on the main page and add in user registration options
-
-render(html` <${App} /> `, document.getElementById("app"));
+export default App;
--- src/Services/restaurant.service.js
+++ src/Services/restaurant.service.js
@@ -1,9 +1,18 @@
 import Parse from "parse";
 
 export const getById = (id) => {
-  const reestaurant = Parse.Object.extend("Restaurant");
-  const query = new Parse.Query(reestaurant);
+  const restaurant = Parse.Object.extend("Restaurant");
+  const query = new Parse.Query(restaurant);
   return query.get(id).then((result) => {
     return result;
   });
 };
+
+export const getAllRestaurant = () => {
+    const Restaurant = Parse.Object.extend("Restaurant");
+    const query = new Parse.Query(Restaurant);
+    return query.find().then((results) => {
+      // returns array of Restaurant objects
+      return results;
+    });
+  };

be61255ed0883815b13d7b622b81a7cce85fa393
Devin Lilly <devinlilly@Devins-MacBook-Pro.local>
2022-03-15 12:30:12 -0400

    debugging

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import {
   html,
   render,
 } from "https://unpkg.com/htm/preact/standalone.module.js";
-import { getSpecials } from "./Services/specials.service.js";
-import { Specials } from "./Components/Specials.js";
-import { Header } from "./Components/Header.js";
+//import {  } from "./Services/specials.service.js";
+import { Specials } from "./Components/Specials/Specials.js";
+import { Header } from "./Components/Header/Header.js";
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import * as Env from "./enviornment";
 import Parse from "parse";
 
-import { getById } from './Common/LearnService';
+import { getById } from './Services/restaurant.service.js';
 
 
 Parse.initialize(Env.APPLICATION_ID, Env.JAVASCRIPT_KEY);
@@ -23,9 +23,10 @@ function App() {
   // we will discuss async data in class, for now you can utilize the pattern
 
   useEffect(() => {
-    return getSpecials().then((data) => {
+    return getById().then((data) => { //getSpecials
       setMenu(data);
     });
+    
   }, []);
   // here is where we put everything together
   return html`
@@ -36,4 +37,6 @@ function App() {
   `;
 } // in the future we will be adding in a map function on the main page and add in user registration options
 
-render(html` <${App} /> `, document.getElementById("app"));
+//render(html` <${App} /> `, document.getElementById("app"));
+
+export default App;

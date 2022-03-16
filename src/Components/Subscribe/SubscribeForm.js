fb4cd5c040074c126abca78a6b0fcc099457aabf
bbarton2 <73309443+bbarton2@users.noreply.github.com>
2022-03-16 15:49:39 -0400

    cereated sub modules for the form on subscribe and login

---
+++ src/Components/Subscribe/SubscribeForm.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+
+const SubscribeFormModule = () => {
+    return (
+      <div>
+        <form>
+        <p>
+          First Name:
+          <input type="text" name="first" size="12" />
+        </p>
+        <p>
+          Last Name:
+          <input type="text" name="last" size="12" />
+        </p>
+        <p>
+          Email:
+          <input type="email" name="email" size="12" />
+        </p>
+        <p>
+          Phone Number:
+          <input type="Phone" name="phonenumber" size="12" />
+        </p>
+        <p>
+          Enter Password:
+          <input type="password" name="password" id="password" size="12" />
+        </p>
+        <p>
+          Re-enter Password:
+          <input type="password" name="password" size="12" />
+        </p>
+        <input type="checkbox" name="subscription" size="10" /> Sign up for
+        promotional emails
+        <br />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      </div>
+    );
+  };
+  
+  export default SubscribeFormModule;
